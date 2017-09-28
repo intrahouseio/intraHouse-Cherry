e74e3f5d430628dac02bd1b6f217af81b527481f
Артем Иванов <fast0490f@mail.ru>
2017-09-28 19:19:22 +0300

    update install.sh

--- install.sh
+++ install.sh
@@ -2,15 +2,33 @@
 
 repo_name="Doc"
 
-echo "Installing IntraHouse-system ..."
+echo -e "\033[0;34m"
+cat << "EOF"
+ ______          __                   __  __
+/\__  _\        /\ \__               /\ \/\ \
+\/_/\ \/    ___\ \ ,_\  _ __    __  \ \ \_\ \    ___   __  __    ____     __
+  \ \ \   /' _ `\ \ \/ /\`'__\/'__`\ \ \  _  \  / __`\/\ \/\ \  /',__\  /'__`\
+   \_\ \__/\ \/\ \ \ \_\ \ \//\ \L\.\_\ \ \ \ \/\ \L\ \ \ \_\ \/\__, `\/\  __/
+   /\_____\ \_\ \_\ \__\\ \_\\ \__/.\_\\ \_\ \_\ \____/\ \____/\/\____/\ \____\
+   \/_____/\/_/\/_/\/__/ \/_/ \/__/\/_/ \/_/\/_/\/___/  \/___/  \/___/  \/____/
+
+----------------------------------------------------------------------------------
+
+EOF
+echo -e "\033[0;36m"
+
+echo -e "...installing \033[0m"
+echo ""
+
 echo "check latest"
 
 file=$(curl -s https://api.github.com/repos/intrahouseio/$repo_name/releases/latest | grep browser_download_url | cut -d '"' -f 4)
 
-echo "latest found: $file"
-echo "get latest ..."
+echo -e "latest found: \033[0;32m $file \033[0m"
+echo "get latest"
+
 curl -sL -o intrahouse-lite.zip $file
 
-echo
+echo -e "\033[0;36m"
 echo "Install complete!!!"
-echo
+echo -e "\033[0m"
