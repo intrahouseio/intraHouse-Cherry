2c61645638ef15724b54fcc0a2de202f7ae92fa4
Артем Иванов <fast0490f@mail.ru>
2017-09-28 18:58:01 +0300

    update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 repo_name="Doc"
+
+echo "Installing IntraHouse-system ..."
+echo "check latest"
+
 file=$(curl -s https://api.github.com/repos/intrahouseio/$repo_name/releases/latest | grep browser_download_url | cut -d '"' -f 4)
 
-echo "Installing IntraHouse..."
-echo "check latest"
 echo "latest found:$file"
-echo "get latest..."
+echo "get latest ..."
 curl -sL -o intrahouse-lite.zip $file
 
 echo
