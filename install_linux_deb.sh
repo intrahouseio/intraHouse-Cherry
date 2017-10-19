b65b68487e9cb1c948094bd3afe78e4e4aa0102f
Артем Иванов <fast0490f@mail.ru>
2017-10-19 11:24:10 +0300

    enable autostart service

--- install_linux_deb.sh
+++ install_linux_deb.sh
@@ -184,105 +184,105 @@ if [[ $type_service == "sysv" ]]; then
   chmod 755 $path_service
 
   cat > $path_service <<EOF
-    #!/bin/sh
-    ### BEGIN INIT INFO
-    # Provides:          $name_service
-    # Required-Start:    \$local_fs \$network \$remote_fs \$syslog \$named \$time
-    # Required-Stop:     \$local_fs \$network \$remote_fs \$syslog \$named
-    # Default-Start:     2 3 4 5
-    # Default-Stop:      0 1 6
-    # Short-Description: Start daemon at boot time
-    # Description:       Enable service provided by daemon.
-    ### END INIT INFO
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:          $name_service
+# Required-Start:    \$local_fs \$network \$remote_fs \$syslog \$named \$time
+# Required-Stop:     \$local_fs \$network \$remote_fs \$syslog \$named
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start daemon at boot time
+# Description:       Enable service provided by daemon.
+### END INIT INFO
 
-    dir="/opt/$name_service"
-    cmd="/opt/$name_service/node/bin/node /opt/$name_service/backend/app.js prod"
-    user=""
+dir="/opt/$name_service"
+cmd="/opt/$name_service/node/bin/node /opt/$name_service/backend/app.js prod"
+user=""
 
-    name=\`basename \$0\`
-    pid_file="/var/run/\$name.pid"
-    stdout_log="/var/log/\$name.log"
-    stderr_log="/var/log/\$name.err"
+name=\`basename \$0\`
+pid_file="/var/run/\$name.pid"
+stdout_log="/var/log/\$name.log"
+stderr_log="/var/log/\$name.err"
 
-    get_pid() {
-        cat "\$pid_file"
-    }
+get_pid() {
+    cat "\$pid_file"
+}
 
-    is_running() {
-        [ -f "\$pid_file" ] && ps -p \`get_pid\` > /dev/null 2>&1
-    }
+is_running() {
+    [ -f "\$pid_file" ] && ps -p \`get_pid\` > /dev/null 2>&1
+}
 
-    case "\$1" in
-        start)
-        if is_running; then
-            echo "Already started"
+case "\$1" in
+    start)
+    if is_running; then
+        echo "Already started"
+    else
+        echo "Starting \$name"
+        cd "\$dir"
+        if [ -z "\$user" ]; then
+            sudo \$cmd >> "\$stdout_log" 2>> "\$stderr_log" &
         else
-            echo "Starting \$name"
-            cd "\$dir"
-            if [ -z "\$user" ]; then
-                sudo \$cmd >> "\$stdout_log" 2>> "\$stderr_log" &
-            else
-                sudo -u "\$user" \$cmd >> "\$stdout_log" 2>> "\$stderr_log" &
-            fi
-            echo \$! > "\$pid_file"
+            sudo -u "\$user" \$cmd >> "\$stdout_log" 2>> "\$stderr_log" &
+        fi
+        echo \$! > "\$pid_file"
+        if ! is_running; then
+            echo "Unable to start, see \$stdout_log and \$stderr_log"
+            exit 1
+        fi
+    fi
+    ;;
+    stop)
+    if is_running; then
+        echo -n "Stopping \$name.."
+        kill \`get_pid\`
+        for i in 1 2 3 4 5 6 7 8 9 10
+        # for i in \`seq 10\`
+        do
             if ! is_running; then
-                echo "Unable to start, see \$stdout_log and \$stderr_log"
-                exit 1
+                break
+            fi
+
+            echo -n "."
+            sleep 1
+        done
+        echo
+
+        if is_running; then
+            echo "Not stopped; may still be shutting down or shutdown may have failed"
+            exit 1
+        else
+            echo "Stopped"
+            if [ -f "\$pid_file" ]; then
+                rm "\$pid_file"
             fi
         fi
-        ;;
-        stop)
-        if is_running; then
-            echo -n "Stopping \$name.."
-            kill \`get_pid\`
-            for i in 1 2 3 4 5 6 7 8 9 10
-            # for i in \`seq 10\`
-            do
-                if ! is_running; then
-                    break
-                fi
-
-                echo -n "."
-                sleep 1
-            done
-            echo
-
-            if is_running; then
-                echo "Not stopped; may still be shutting down or shutdown may have failed"
-                exit 1
-            else
-                echo "Stopped"
-                if [ -f "\$pid_file" ]; then
-                    rm "\$pid_file"
-                fi
-            fi
-        else
-            echo "Not running"
-        fi
-        ;;
-        restart)
-        \$0 stop
-        if is_running; then
-            echo "Unable to stop, will not attempt to start"
-            exit 1
-        fi
-        \$0 start
-        ;;
-        status)
-        if is_running; then
-            echo "[\033[0;32m ok \033[0m] \$name is running."
-        else
-            echo "[\033[0;31m FAIL \033[0m] \$name is not running ... \033[0;31mfailed! \033[0m]"
-            exit 1
-        fi
-        ;;
-        *)
-        echo "Usage: \$0 {start|stop|restart|status}"
+    else
+        echo "Not running"
+    fi
+    ;;
+    restart)
+    \$0 stop
+    if is_running; then
+        echo "Unable to stop, will not attempt to start"
         exit 1
-        ;;
-    esac
+    fi
+    \$0 start
+    ;;
+    status)
+    if is_running; then
+        echo "[\033[0;32m ok \033[0m] \$name is running."
+    else
+        echo "[\033[0;31m FAIL \033[0m] \$name is not running ... \033[0;31mfailed! \033[0m]"
+        exit 1
+    fi
+    ;;
+    *)
+    echo "Usage: \$0 {start|stop|restart|status}"
+    exit 1
+    ;;
+esac
 
-    exit 0
+exit 0
 EOF
 
 update-rc.d $name_service defaults
