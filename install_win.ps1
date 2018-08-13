ace5424d6fd3f8aa603120a24bbcd8d03042ca87
Artem Ivanov <fast0490f@mail.ru>
2018-08-13 17:49:36 +0300

    Update install_win.ps1

--- install_win.ps1
+++ install_win.ps1
@@ -12,8 +12,8 @@ $port=8088
 $root="$($env:LOCALAPPDATA)\$($name_service)"
 $project_path="C:\ProgramData\$($name_service)\projects\$($project_name)"
 
-if (args[0]) {
-$lang = switch ( args[0]l )
+if ($args[0]) {
+$lang = switch ( $args[0] )
     {
         ru { 'ru' }
         en { 'en' }
