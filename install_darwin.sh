46f42205b4c5231d17a4f49c6ea9cbd6bc9332f3
Artem Ivanov <fast0490f@mail.ru>
2018-10-26 10:05:07 +0300

    update macOS

--- install_darwin.sh
+++ install_darwin.sh
@@ -105,6 +105,8 @@ cat > $path_service <<EOF
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
+  <key>LowPriorityIO</key>
+  <true/>
   <key>RunAtLoad</key>
   <true/>
   <key>KeepAlive</key>
