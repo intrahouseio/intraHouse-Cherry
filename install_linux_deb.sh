61c43fc0057201bc10772588f7c87f8493ebe712
Artem Ivanov <fast0490f@mail.ru>
2019-12-26 12:12:34 +0300

    update

--- install_linux_deb.sh
+++ install_linux_deb.sh
@@ -143,7 +143,7 @@ rm -fr ./node.tar.xz
 
 cd ./backend
 #export PATH=$root/node/bin:$PATH
-sudo sudo $root/node/bin/node $root/node/bin/npm i --only=prod
+$root/node/bin/node $root/node/bin/npm i --only=prod
 
 #-------------- end
 
